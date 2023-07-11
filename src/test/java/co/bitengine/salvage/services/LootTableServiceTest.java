b257ae8cb7ab97578ea02e4929bf39e7265cabdf
burnoutapp <theburnoutapp@gmail.com>
2023-07-10 22:06:31 -0500

    Feat: Initial testing

---
+++ src/test/java/co/bitengine/salvage/services/LootTableServiceTest.java
@@ -0,0 +1,17 @@
+package co.bitengine.salvage.services;
+
+import junit.framework.TestCase;
+
+public class LootTableServiceTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testGenerate() {
+    }
+
+    public void testGetQuantity() {
+    }
+}
